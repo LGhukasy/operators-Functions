12ce1fb796374e1c542ffcd35742609e3494fc3a
Levon <levonghukasy@gmail.com>
2019-12-28 13:02:15 +0400

    add taska

---
+++ index.js
@@ -0,0 +1,102 @@
+// let a = 5 & 4;
+// function fu1(){
+//     console.log(a)
+// };
+// fu1();
+
+// let a = 5 || 4;
+// function fu1(){
+//     console.log(a)
+// };
+// fu1();
+
+// let a = 5 && 4 && 8 || undefined && 15;
+// function fu1(){
+//     console.log(a)
+// };
+// fu1();
+
+// let a = 5 && 4 && (8,0) || undefined && 15;
+// function fu1(){
+//     console.log(a)
+// };
+// fu1();
+
+// let a = 5;
+// function fu1(){
+//     console.log(a++);
+//     console.log(++a);
+// };
+// fu1();
+
+// let a = 5;
+// let b;
+// let c;
+// function fu1(){
+//     b = a++;
+//     c = a++;
+//     console.log(b);
+//     console.log(c);
+//     console.log(a);
+// };
+// fu1();
+
+
+// console.log(65 + "")
+// console.log(89 + "" - 1 + 0)
+// console.log(89 + "sdasd" - 1 + 0)
+// console.log(true + false + undefined)
+// console.log(6 / "3")
+// console.log("2" * "3")
+// console.log(4 + 5 + "px")
+// console.log("$" + 4 + 5)
+// console.log("4" - 2)
+// console.log("4px" - 2)
+// console.log(7 / 0)
+// console.log("  -9  " + 5)
+// console.log("  -9  " - 5)
+// console.log(null + 1)
+// console.log(undefined + 1)
+// console.log(" \t \n" - 2)
+
+// let a = 1;
+// let b = 5;
+// console.log(++a - b * 5)
+
+
+// let a = 1;
+// let b = 5;
+// console.log((++a - b) * 5)
+
+
+// let a = (1, 2+ 5, undefined && 5, 15 || 4);
+// console.log(a)
+
+
+// let a = (1, 2+ 5, undefined && 5, 15 || 4) && (4,5);
+// console.log(a)
+
+
+// function a(){
+//     console.log("hello");
+//     return "Hello1"
+// };
+
+// console.log({false : false} && a())
+
+
+// function a(){
+//     console.log("hello");
+//     return "hello1"
+// };
+
+// console.log({false : false} || a())
+
+
+// // Ստեղ պիտի ջոկես խիա error տալիս(խիա throw քցում)
+// console.log("Hello1")
+// (function(){
+//     console.log("Hello")
+// })()
+
+
